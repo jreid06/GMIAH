c63ca12c8f45a8b22727295797570862c13b96e8
Jason Reid <jasonbrandon9406@gmail.com>
2017-06-22 21:34:36 +0100

    all files for GMIAH

---
+++ index.php
@@ -0,0 +1,303 @@
+<?php
+    if($_POST["submit"]) {
+        
+        $result = "form submitted successfully";
+    }
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>GMIAH | HOME</title>
+
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+      
+    <link rel="stylesheet" type="text/css" href="webfontkit/stylesheet.css"/>
+      
+    <!--Stylesheets-->
+    <link rel="stylesheet" type="text/css" href="main.css"/>
+    <link rel="stylesheet" type="text/css" href="animate.css"/>
+    <link rel="stylesheet" type="text/css" href="form.css"/>
+      
+    <!--Fonts-->
+    <link href='https://fonts.googleapis.com/css?family=Asap:400,700' rel='stylesheet' type='text/css'>
+      
+    <script>
+        var a = document.createElement('div');
+var b = document.createElement('p');
+// Add event listeners to b etc...
+a.appendChild(b);
+a.removeChild(b);
+b = null; 
+// A reference to 'b' no longer exists 
+// Therefore the element and any event listeners attached to it are removed.  
+    </script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+      
+      <style>
+        .wow {
+   visibility: hidden;
+}
+      </style>
+  </head>
+  <body>
+    <div class="container contain01">
+        <div class="row row01">
+            
+            <div class="col-xs-2 col-sm-2 col-md-2">
+                <div class="logo-top">
+                    <img src="img/get-me-into-a-house-logo-2.png"/>
+                </div>
+            </div>
+          
+        <nav class="navbar">
+            
+              <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                  
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul class="nav navbar-nav">
+                    <li class="active"><a href="index.php">HOME <span class="sr-only">(current)</span></a></li>
+                    <li><a href="buyahouse.html">BUY A HOUSE</a></li>
+                     <li><a href="#">AVAILABLE LISTINGS</a></li>
+                      <li><a href="apply.html">APPLY ONLINE</a></li>
+                      <li><a href="earn-cash.html">YOU CAN EARN MONEY</a></li>
+                     <li><a href="testimonials.html">TESTIMONIALS</a></li>
+                    <li><a href="about.html">ABOUT US</a></li>
+                    <li><a href="contact.html">CONTACT US</a></li>
+                    <li><a href="faq.html">FAQ</a></li>
+                  
+                  </ul>
+                  <!--<form class="navbar-form navbar-left" role="search">
+                    <div class="form-group">
+                      <input type="text" class="form-control" placeholder="Search">
+                    </div>
+                    <button type="submit" class="btn btn-default">Submit</button>
+                  </form>
+                  <ul class="nav navbar-nav navbar-right">
+                    
+                  </ul>-->
+                </div><!-- /.navbar-collapse -->
+              </div><!-- /.container-fluid -->
+            </nav>
+        </div> <!--end of row01-->
+    </div><!--End of contain01-->
+      
+    <!--MOBILE CONTENT-->  
+      
+    <div class="container contain02">
+        <div class="wrapper">
+            <div class="row row02">
+                <div class="col-xs-12 col-sm-6 col-md-6">
+                    <div class="section1">
+                       <div class="image-h1">
+                            <div class="textH1">
+                                <h3>YOU DESERVE YOUR OWN<span class="tCol2"> HOME</span></h3>
+                                <a href=""><h5>Let us show you with as little as &pound;5000 down <span class="fa fa-arrow-circle-o-right ar"></span></h5></a>
+                            </div><!--end of texth1-->
+                            
+                            <img src="img/yellow-bground-woman.jpeg" alt="yellow wall with a woman"/>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-xs-4 col-sm-3 col-md-3 hidden-xs">
+                    <div class="section2">
+                        <div class="image-h2">
+                            
+                           <img src="img/photo-1464566024458-540310eec1a5.jpeg"/>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-xs-4 col-sm-3 col-md-3 hidden-xs">
+                    <div class="section3">
+                        <div class="image-h3">
+                           <img src="img/photo-1442124920820-8e5f4b07b563.jpeg"/>
+                        </div>
+                        <div class="image-h4">
+                           <img src="img/photo-1464564531096-f0163633a18a.jpeg"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row row03">
+                <div class="col-xs-12 col-md-12">
+                    <hr class="small">
+                </div>
+            </div>
+
+            <div class="row row04">
+                <div class="col-xs-12 col-md-4">
+                    <div class="section4 blueCol">
+                        <div class="home-info hInfo-01">
+                            <h3><span class="tCol">“&pound;5000</span> Down &amp; You’re In”</h3>
+                            
+                            <p>For instance, our <em>"&pound;5000 Down &amp; You’re In"</em> program has helped scores of people to get into their own home with far less money than what the “experts“ said they needed. <a href="#">You can read more about that program by clicking here</a></p>
+                            
+                            <hr class="small">
+                        </div>
+                    </div>
+                </div>
+                
+                 <div class="col-xs-12 col-md-4">
+                    <div class="section5 middleBG-home">
+                        <div class="image-h5">
+                            
+                        </div>
+                     </div>
+                </div>
+                
+                <div class="col-xs-12 col-md-4 ">
+                    <div class="section6 blueCol">
+                        <div class="home-info hInfo-02">
+                            <h3>Rent to <span class="tCol">Own</span></h3>
+                            
+                            <p>We also have a great <a href="#">"Rent to Own"</a> program. It does not require bank qualifying. You could be living in your own home in just a few days from now! We’ll work together to repair any credit problems AFTER you move in, so you can get a regular mortgage down the road.<a href="#"> Click here for more information on this new, creative solution.</a></p>
+                            
+                            <p>Imagine your own home...a nice yard...no apartment neighbors...even your own laundry room and garage! Not to mention the tax benefits of owning your own home, and the profit potential from home prices going up. If you’ve been looking for a way to get all those benefits, we’re the solution.</p>
+                            
+                             
+                            
+                        </div>
+                        <hr class="small">
+                     </div>
+                </div>
+            </div>
+            
+            <div class="row row05">
+                
+                 <div class="col-xs-12 col-sm-12  col-md-12">
+                     <div class="section5">
+                        <div class="video-home">
+                            <div class="embed-responsive embed-responsive-16by9">
+                              <script src="//fast.wistia.com/embed/medias/wgl34tdw7u.jsonp" async></script><script src="//fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_wgl34tdw7u videoFoam=true" style="height:100%;width:100%">&nbsp;</div></div></div>
+                            </div>
+                        </div>
+                     </div>
+                      <hr class="small">
+                </div>
+            </div>
+            
+            <div class="row row06">
+                
+                 <div class="col-xs-12 col-sm-12 col-md-offset-0 col-md-7">
+                     <div class="section5">
+                        <div class="home-info hInfo-03">     
+                            <h1><span class="tCol">Free</span> Special Report</h1>
+                            
+                            <h4>This report will open your eyes to the options other people have never told you about. Or just click here if you would like us to review your situation, and tell you right away if we can help to make your dream come true!</h4>
+                            
+                            <p><a href="#">Click here to have us mail you a Free Special Report</a>, called</p>
+                            
+                            <h3>“How to Own A Home When Others Say You Cant!”</h3>
+                            
+                            
+                            
+                            <p>You don’t have to make any commitment now...we respect that you simply want more information. But contacting us for our Free Specical Report or our No-Obligation Review will be the smartest move you could possibly make!</p>
+                            
+                             <hr class="small">
+                            
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-0 col-md-4 smPlus-edit">
+                    
+                   <form id="contact-form" class="form formH" action="#" method="POST" role="form">
+                                      
+                                    <div class="form-group">
+                                      <label class="form-label" for="name">First Name</label>
+                                      <input type="text" class="form-control" id="fName" name="fName" placeholder="First name" tabindex="1" required>
+                                    </div>
+                                    
+                                    <!--form section divider-->
+                                      
+                                    
+                                    <!--form section divider-->
+                                      
+                                    <div class="form-group">
+                                      <label class="form-label" for="email">Your Email</label>
+                                      <input type="email" class="form-control" id="email" name="email" placeholder="Your Email" tabindex="2" required>
+                                    </div>
+                                    
+                                    <!--form section divider-->
+                                    
+                              
+                                    <div class="form-group">
+                                      <label class="form-label" for="name">Mobile Number</label>
+                                      <input type="text" class="form-control" id="m-number" name="m-number" placeholder="Last name" tabindex="1" required>
+                                    </div>
+                                      
+                                    <!--form section divider-->
+                                      
+                                     <!--form section divider-->  
+                                    
+                                    <div class="text-center">
+                                      <button type="submit" id="submit" name="submit" class="btn btn-start-order">Send Message</button>
+                                    </div>
+                       
+                                    <?php echo $result; ?>
+                                  </form>
+                    
+                </div>
+            </div>
+
+          
+    
+        </div><!--wrapper end-->
+        <div class="footer">
+            <div class="row rowf1">
+                
+            </div>
+            <div class="row rowf2">
+                <div class="col-xs-12">
+                    <p>© 2016 A Fast Sale. Your Direct Contact to Buyers, Sellers Properties &amp; Funding</p>
+                    <p>Web Design by <a href="http://www.jbrwebdesign.co.uk" target="_blank">JBR Web Design</a></p>
+                </div>
+            </div>
+        </div>
+    </div><!--container end-->
+    
+    <!--MOBILE CONTENT END-->  
+      
+    
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+      
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+      
+    <script src="js/wow.js"></script>
+              <script>
+              new WOW().init();
+    </script>
+  </body>
+</html>
